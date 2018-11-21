9e011ddef0902726f8257ca3ba0a8366f4f637dc
Veysel <veysel.dogan@ogr.sakarya.edu.tr>
2018-11-21 13:37:42 +0300

    Initial Commit

---
+++ Odevdenemesi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Odevdenemesi
+//
+//  Created by Veysel on 21.11.2018.
+//  Copyright © 2018 Veysel. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
