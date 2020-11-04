9797cf882e11612e82f2e4775d0a38b419b0ff8d
veyselgithub <67361933+veyselgithub@users.noreply.github.com>
2020-11-04 20:15:34 +0300

    Parantez Odevi

--- Odevdenemesi/main.cpp
+++ Odevdenemesi/main.cpp
@@ -7,9 +7,46 @@
 //
 
 #include <iostream>
+#include <stack>
+using namespace std;
+char uyari(string ifade){
+	stack<char>t;
+	for (int i=0; i<ifade.length(); i++) {
+		switch (ifade[i]) {
+			case '(':
+				t.push(ifade[i]);
+				break;
+			case')':
+				if(t.size()==0||t.top()!='(')
+					return ')';
+				else
+					t.pop();
+		}
+	}
+if(t.size()>0)
+	return  t.top();
+	else
+		return '-';
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	string ifade="";
+	cout<<"Ifade girin";
+	cin>>ifade;
+	
+	switch (uyari(ifade)) {
+		case'-':
+			cout<<"Ifade de parantez hatasi yok ";
+			break;
+		case'(':
+			cout<<"Ifade de  ) hatasi var ";
+			break;
+		case')':
+			cout<<"Ifade de ( hatasi var ";
+			break;
+			
+		
+	}
+	
 	return 0;
 }
